56945db842dbbccb27353a6ad8b5760e90d8c9b8
Kuzma02 <aleksandarkuzmanovic02@gmail.com>
2024-06-18 15:24:43 +0200

    starting with chartjs

---
+++ src/components/chart/Line.tsx
@@ -0,0 +1,6 @@
+const Line = () => {
+  return (
+    <div>Line</div>
+  )
+}
+export default Line
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,9 +1,11 @@
 import { Sidebar } from "../components"
+import Line from "../components/chart/Line"
 
 const Landing = () => {
   return (
     <div className="h-[100vh] border-t border-blackSecondary border-1">
       <Sidebar />
+      <Line />
     </div>
   )
 }
